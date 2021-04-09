15aa1a16100d2104d3f16a422b670be68a7016d7
Guillaume P <gcconantc@gmail.com>
2021-04-09 14:14:06 +0200

    QSettings group

--- qc3-1/main.cpp
+++ qc3-1/main.cpp
@@ -2,6 +2,34 @@
 #include <QSettings>
 #include <QDebug>
 
+void saveAge(QSettings* settings, QString group, QString name, int age) {
+    settings->beginGroup(group);
+    settings->setValue(name, age);
+    settings->endGroup();
+}
+
+int getAge(QSettings* settings, QString group, QString name) {
+    settings->beginGroup(group);
+
+    if (!settings->contains(name)) {
+        qWarning() << "Settings does not contain " << group << " : " << name;
+        settings->endGroup();
+        return 0;
+    }
+
+    bool ok;
+    int value = settings->value(name).toInt(&ok);
+    settings->endGroup();
+
+    if (!ok) {
+        qWarning() << "Failed to convert to int";
+        settings->endGroup();
+        return 0;
+    }
+
+    return value;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -19,8 +47,16 @@ int main(int argc, char *argv[])
     //settings.setValue("test", 123);
 
     //read
-    qInfo() << settings.value("test").toString();
-    qInfo() << settings.value("test").toInt();
+    //qInfo() << settings.value("test").toString();
+    //qInfo() << settings.value("test").toInt();
+
+    saveAge(&settings, "people", "tintin", 16);
+    saveAge(&settings, "whisky", "bowmore", 12);
+    saveAge(&settings, "whisky", "tintin", 2);
+
+    qInfo() << getAge(&settings, "people", "tintin");
+    qInfo() << getAge(&settings, "whisky", "bowmore");
+    qInfo() << getAge(&settings, "whisky", "tintin");
 
     return a.exec();
 }
