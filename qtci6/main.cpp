a7ae32b5ee8db189dd362f092c965a4b9e55daae
Guillaume P <gcconantc@gmail.com>
2021-04-12 11:10:13 +0200

    Base64

--- qtci6/main.cpp
+++ qtci6/main.cpp
@@ -2,7 +2,8 @@
 #include <QDebug>
 #include <QFile>
 #include <QDir>
-
+#include <QTextStream>
+#include <QRandomGenerator>
 
 QByteArray makeData() {
     QByteArray data;
@@ -19,22 +20,69 @@ QByteArray makeData() {
     }
     return data;
 }
+
+QString makeDataUnicode() {
+    QString data;
+
+    data.append("Unicode test \r\n");
+
+    for (int i = 0; i < 10; i++) {
+        int number = QRandomGenerator::global()->bounded(1112064);
+        data.append(number);
+    }
+
+    data.append("\r\n");
+
+    return data;
+}
+
+QString makeDataBase64() {
+    QString data;
+
+    data.append("Unicode test \r\n");
+
+    for (int i = 0; i < 10; i++) {
+        data.append("Hello \r\n");
+    }
+
+    data.append("\r\n");
+
+    return data;
+}
+
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QByteArray data = makeData();
+    //QByteArray data = makeData();
+    QString data = makeDataUnicode();
 
     QString path = QDir::currentPath() + QDir::separator() + "test.txt";
     QFile file(path);
 
     if (file.open(QIODevice::WriteOnly)) {
-        file.write(data);
+
+        QTextStream stream(&file);
+        stream.setCodec("UTF-8");
+        stream << data;
+        stream.flush();
 
         file.close();
     }
 
     qInfo() << "Done";
+    qInfo() << "Unicode :" << data;
+    qInfo() << "ASCII " << data.toLatin1();
+
+    QString data64 = makeDataBase64();
+    QByteArray bytes(data.toLatin1());
+    QByteArray encoded = bytes.toBase64();
+    QByteArray decoded = bytes.fromBase64(encoded);
+
+    qInfo() << "Encoded " << encoded;
+    qInfo() << "Decoded " << decoded;
 
     return a.exec();
 }
