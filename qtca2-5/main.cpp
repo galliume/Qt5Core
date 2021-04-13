78f73c45e6139865cee66ebfea268aff8ac7230c
Guillaume P <gcconantc@gmail.com>
2021-04-13 20:54:43 +0200

    QTcpSocket

--- qtca2-5/main.cpp
+++ qtca2-5/main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include "client.h"
+#include <QTcpSocket>
 
 // /!\ add <QT += network> to project file
 
@@ -18,6 +19,34 @@ int main(int argc, char *argv[])
     Client client;
     //client.connectToHost("httpbin.org", 80);
     client.connectToHost("httpbin.org", 443);
+
+    QTcpSocket socket;
+    socket.connectToHost("voidrealms.com", 80);
+    qInfo() << "Connecting";
+
+    if (socket.waitForConnected(50000)) {
+        qInfo() << "Connected sending request";
+
+        QByteArray data;
+        data.append("GET /get HTTP/1.1\r\n");
+        data.append("Host: local\r\n");
+        data.append("Connection: Close\r\n");
+        data.append("\r\n");
+
+        socket.write(data);
+        socket.waitForBytesWritten();
+
+        if (socket.waitForReadyRead(10000)) {
+            QByteArray response = socket.readAll();
+            qInfo() << "Response " << response.length() << " bytes";
+        }
+
+        //socket.close();
+        socket.waitForDisconnected();
+        qInfo() << "Done";
+    } else {
+        qInfo() << socket.errorString();
+    }
 
     return a.exec();
 }
