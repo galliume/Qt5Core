4e80c86acaa1eabd50d86046e09cb1f4fbbb00c0
Guillaume P <gcconantc@gmail.com>
2021-04-13 19:25:57 +0200

    QNetworkProxy

--- qtca2-5/client.cpp
+++ qtca2-5/client.cpp
@@ -8,6 +8,12 @@ Client::Client(QObject *parent) : QObject(parent)
     connect(&socket, &QTcpSocket::stateChanged, this, &Client::stateChanged);
     connect(&socket, &QTcpSocket::readyRead, this, &Client::readyRead);
     connect(&socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error), this, &Client::error);
+
+    QNetworkProxy proxy(QNetworkProxy::HttpProxy, "165.225.77.47", 9400);
+//    proxy.setUser("galliume");
+//    proxy.setPassword("password");
+    //QNetworkProxy::setApplicationProxy(proxy);
+    socket.setProxy(proxy);
 }
 
 void Client::connectToHost(QString host, quint16 port)
@@ -26,10 +32,17 @@ void Client::disconnect()
 
 void Client::connected()
 {
-    socket.write("HELLO\r\n");
+    //socket.write("HELLO\r\n");
+    qInfo() << "Socket sending";
+    QByteArray data;
+    data.append("GET /get HTTP/1.1\r\n");
+    data.append("User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)\r\n");
+    data.append("Host: local\r\n");
+    data.append("Connection: Close\r\n");
+    data.append("\r\n");
+    socket.write(data);
 
-    qInfo() << "Socket connected";
-
+    socket.waitForBytesWritten();
 }
 
 void Client::disconnected()
--- qtca2-5/main.cpp
+++ qtca2-5/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     Client client;
-    client.connectToHost("www.voidrealms.com", 80);
+    client.connectToHost("httpbin.org", 80);
 
     return a.exec();
 }
